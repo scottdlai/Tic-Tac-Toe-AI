01928c12a8659a78f5c1fd1d8e93d6949a2bb852
Scott Lai <tuonglai3602@gmail.com>
2020-12-27 17:30:48 -0800

    Updates

---
+++ precache-manifest.6ec54555f491f3da5bd38a9bed996658.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "923f4e1753ded0c456193e1355545dbb",
+    "url": "/Tic-Tac-Toe-AI/index.html"
+  },
+  {
+    "revision": "2191e50313466335ab9c",
+    "url": "/Tic-Tac-Toe-AI/static/css/main.32d1f279.chunk.css"
+  },
+  {
+    "revision": "44cac84b209f70257e97",
+    "url": "/Tic-Tac-Toe-AI/static/js/2.bb21a5fe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Tic-Tac-Toe-AI/static/js/2.bb21a5fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2191e50313466335ab9c",
+    "url": "/Tic-Tac-Toe-AI/static/js/main.f3f30bb5.chunk.js"
+  },
+  {
+    "revision": "4b40c1b7ca9e48f55078",
+    "url": "/Tic-Tac-Toe-AI/static/js/runtime-main.96b8850c.js"
+  }
+]);
